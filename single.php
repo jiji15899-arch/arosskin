ce74edec7be7bc48a76db919422613d5f5d86ffe
jiji15899-arch <jiji15899@gmail.com>
2026-01-19 19:45:20 +0900

    Update single.php

--- single.php
+++ single.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * 글쓰기 스킨 - 글 상세 페이지
+ * 글쓰기 스킨 - 글 상세 페이지 (수정됨)
  */
 
 get_header();
@@ -14,7 +14,11 @@ while (have_posts()) : the_post();
     
     <?php if (is_singular('post')) : ?>
     <header class="entry-header">
-        <h1 class="entry-title" style="display: none;"><?php the_title(); ?></h1>
+        <div class="entry-meta-top">
+            <span class="post-category"><?php the_category(', '); ?></span>
+            <span class="post-date"><?php echo get_the_date('Y.m.d'); ?></span>
+        </div>
+        <h1 class="entry-title"><?php the_title(); ?></h1>
     </header>
     <?php endif; ?>
     
@@ -29,6 +33,12 @@ while (have_posts()) : the_post();
         ?>
     </div>
     
+    <?php if (has_tag()) : ?>
+    <div class="entry-tags">
+        <?php the_tags('', '', ''); ?>
+    </div>
+    <?php endif; ?>
+
 </article>
 
 <?php
